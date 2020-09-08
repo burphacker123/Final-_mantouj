14ec23dde4a4069fc00dc91e04963625e1648db2
unknown <amned2004@gmail.com>
2020-09-08 10:35:01 +0200

    fg

--- princ/views.py
+++ princ/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    return render(request, 'index.html')
+def about(request):
+    return render(request, 'about.html')
+def natural(request):
+    return render(request, 'shop.html')
+def artisanal(request):
+    return render(request, 'artisanal.html')
+def contact(request):
+    return render(request, 'contact.html')
